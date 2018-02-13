e2ba952b4676561ff7aa12aff16f98baa6007496
Alberto Juan Luis Reina Pineda <areina@tigo.com.gt>
2018-02-13 08:39:20 -0600

    Cambio de Archivos para Jenkins

--- plancolaborador/ContratosPlanColaborador/src/com/tigo/contrato/plancolaborador/model/Contrato.java
+++ plancolaborador/ContratosPlanColaborador/src/com/tigo/contrato/plancolaborador/model/Contrato.java
@@ -19,7 +19,7 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import java.text.SimpleDateFormat;
 
 
-//ADDED A COMMENT IN PROGRAM - ANOTHER TEST
+//ADDED A COMMENT IN PROGRAM - ANOTHER TEST FOR JENKINS
 public class Contrato {
 
 	private File archivoBase;
