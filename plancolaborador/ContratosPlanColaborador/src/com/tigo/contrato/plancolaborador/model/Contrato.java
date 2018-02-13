b6bd912986ef00efaf7439e2d4fed76acbefa3be
Alberto Juan Luis Reina Pineda <areina@tigo.com.gt>
2018-02-13 08:29:53 -0600

    added files

--- plancolaborador/ContratosPlanColaborador/src/com/tigo/contrato/plancolaborador/model/Contrato.java
+++ plancolaborador/ContratosPlanColaborador/src/com/tigo/contrato/plancolaborador/model/Contrato.java
@@ -19,7 +19,7 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import java.text.SimpleDateFormat;
 
 
-//ADDED A COMMENT
+//ADDED A COMMENT IN PROGRAM
 public class Contrato {
 
 	private File archivoBase;
